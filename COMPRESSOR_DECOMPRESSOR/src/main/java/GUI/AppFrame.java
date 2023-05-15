feb13421ecea6171db5412a29616c96deba6a2d2
FARHAT NASIM <118013560+FARHATNASIM@users.noreply.github.com>
2023-05-15 23:22:36 +0530

    init

---
+++ COMPRESSOR_DECOMPRESSOR/src/main/java/GUI/AppFrame.java
@@ -0,0 +1,72 @@
+package GUI;
+
+
+import comp_decomp.compressor;
+import comp_decomp.decompressor;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+public class AppFrame extends JFrame implements ActionListener {
+    JButton compressButton;
+    JButton decompressButton;
+
+    AppFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        compressButton = new JButton("Select file to compress");
+        compressButton.addActionListener(this);
+        compressButton.setBounds(20, 120, 200, 30);
+
+        decompressButton = new JButton("Select file to decompress");
+        decompressButton.addActionListener(this);
+        decompressButton.setBounds(260, 120, 200, 30);
+
+        this.add(compressButton);
+        this.add(decompressButton);
+        this.setSize(500, 600);
+        this.getContentPane().setBackground(Color.BLACK);
+        this.setVisible(true);
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == compressButton) {
+            JFileChooser fileChooser = new JFileChooser();
+            int response = fileChooser.showSaveDialog(null);
+
+            if (response == JFileChooser.APPROVE_OPTION) {
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                System.out.print(file);
+                try {
+                    compressor compress=new compressor();
+                    compress.method(file);
+                } catch (Exception ee) {
+                JOptionPane.showMessageDialog(null, ee.toString());
+                }
+            }
+        }
+
+
+        if (e.getSource() == decompressButton) {
+            JFileChooser fileChooser = new JFileChooser();
+            int response = fileChooser.showSaveDialog(null);
+
+            if (response == JFileChooser.APPROVE_OPTION) {
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                System.out.print(file);
+                try {
+                	
+                    decompressor decompress=new decompressor();
+                    decompress.method(file);
+                } catch (Exception ee) {
+                    JOptionPane.showMessageDialog(null, ee.toString());
+                }
+            }
+        }
+    }
+}
